2942e57279fead247ae29269f2f352dc676052ee
Douglas Scriptore <douglass@rpc.com.br>
2020-09-08 11:30:56 -0300

    chore: add axios lib and create context react logic

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, useCallback, useState } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentails: SignInCredentials): Promise<void>;
+}
+
+// como digo que AuthContext deve ter nome, para autenticação não tem sentido
+// então fazendo da forma abaixo {} as AuthContextData vocẽ burla o TS
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  // o estado inicial vai buscar no localStorage se já existe algum dado
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', { email, password });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
